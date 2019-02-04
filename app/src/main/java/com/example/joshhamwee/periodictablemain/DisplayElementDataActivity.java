40e13565ce819d586fa2c15d029b8217cd530f5a
Andrei Bogdan <bogda036.304@gmail.com>
2019-02-04 22:19:10 +0000

    further updated design of element page

--- app/src/main/java/com/example/joshhamwee/periodictablemain/DisplayElementDataActivity.java
+++ app/src/main/java/com/example/joshhamwee/periodictablemain/DisplayElementDataActivity.java
@@ -11,15 +11,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v4.content.ContextCompat;
 
+import java.nio.channels.NotYetBoundException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 
 public class DisplayElementDataActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     //NavigationView navigationView;
-
+    //Lists of different elements to determine colours
+    private static final Set<Integer> AlkMetals = new HashSet<>(Arrays.asList(new Integer[]{1,3,11,19,37,55,87}));
+    private static final Set<Integer> AlkEMetals = new HashSet<>(Arrays.asList(new Integer []{4,12,20,38,56,88}));
+    private static final Set<Integer> TranMetals = new HashSet<>(Arrays.asList(new Integer []{21,22,23,24,25,26,27,28,29,30,
+    39,40,41,42,43,45,46,47,48,72,73,74,75,76,77,78,79,80}));
+    private static final Set<Integer> Metaloids = new HashSet<>(Arrays.asList(new Integer []{13,31,32,49,50,51,81,82,83,84}));
+    private static final Set<Integer> NonMetals = new HashSet<>(Arrays.asList(new Integer []{5,6,7,8,9,14,15,16,17,33,34,35,52,53,85}));
+    private static final Set<Integer> NobleGases = new HashSet<>(Arrays.asList(new Integer []{2,10,18,36,54,86}));
+    //redundant --> may find another use later remove if not
+   // private static final Set<Integer> HeavyEl = new HashSet<>(Arrays.asList(new Integer []{57,58,59}))
 
 
     @Override
@@ -54,18 +68,34 @@ public class DisplayElementDataActivity extends AppCompatActivity {
         TextView AtomicMass = findViewById(R.id.AtomicMass);
         AtomicMass.setText("Atomic Mass: " + currentElement.atomicMass);
 
-        //need to set up different text colours matching colours in table
-        if(atomicNumber == 1) {
-            ElementName.setTextColor(getResources().getColor(R.color.pastelPink));
+
+        //should probably define a separate function to set up colour-- too much repetition
+        if(AlkMetals.contains(atomicNumber)) {
+            ElementName.setTextColor(ContextCompat.getColor(this, R.color.pastelPink));
         }
-        else if(atomicNumber == 4){
-            ElementName.setTextColor(getResources().getColor(R.color.pastelOrange));
+        else if(AlkEMetals.contains(atomicNumber)){
+            ElementName.setTextColor(ContextCompat.getColor(this,R.color.pastelOrange));
         }
-        else
-            ElementName.setTextColor(getResources().getColor(R.color.darkpurple));
+        else if(TranMetals.contains(atomicNumber)){
+            ElementName.setTextColor(ContextCompat.getColor(this,R.color.pastelYellow));
+        }
+        else if(Metaloids.contains(atomicNumber)){
+            ElementName.setTextColor(ContextCompat.getColor(this,R.color.pastelBlue));
+        }
+        else if(NonMetals.contains(atomicNumber)){
+            ElementName.setTextColor(ContextCompat.getColor(this,R.color.pastelPurple));
+        }
+        else if(NobleGases.contains(atomicNumber)){
+            ElementName.setTextColor(ContextCompat.getColor(this,R.color.pastelGrey));
+        }
+        else{
+            ElementName.setTextColor(ContextCompat.getColor(this,R.color.pastelGreen));
+        }
     }
 
     /********** this is repeated code, probs should find a way around it. hehh..... ***/
+
+
     private void setUpToolbar() {
         //Find the toolbar by the specific id
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
