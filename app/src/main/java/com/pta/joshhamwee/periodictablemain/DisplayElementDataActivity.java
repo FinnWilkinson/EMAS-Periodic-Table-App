43dc46a6538b5a8946cc5dadc64154cab227dcff
Andrei Bogdan <bogda036.304@gmail.com>
2019-04-29 09:56:27 +0100

    removed some sheeet

--- app/src/main/java/com/pta/joshhamwee/periodictablemain/DisplayElementDataActivity.java
+++ app/src/main/java/com/pta/joshhamwee/periodictablemain/DisplayElementDataActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
